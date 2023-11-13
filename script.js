6cdfe7e5525bc06bdb6a46111c0512b8e3201948
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-13 16:56:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
-let firstVar=prompt();
-let result=firstVar*2;
+
+let firstVar=parseInt(prompt());
+let result=firstVar*2
 let secondVar=parseInt(prompt());
  result=result+secondVar;
-alert(result);
+alert(`"The final result is: ${result}"`);
+
